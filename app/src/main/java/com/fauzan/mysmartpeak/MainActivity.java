1d8864783570b091b6754ee0c79052108139b69c
Fauzan <muhdfauzan9302@gmail.com>
2020-02-17 19:33:40 +0800

    Code refactor

--- app/src/main/java/com/fauzan/mysmartpeak/MainActivity.java
+++ app/src/main/java/com/fauzan/mysmartpeak/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 JSONArray printValues = new JSONArray();
                 printValues.put(EzSmartpeak.PrintLogo("jpg", EzSmartpeak.TEXT_CENTER));
-                printValues.put(EzSmartpeak.PrintText("MobilityOne Sdn Bhd", 2, EzSmartpeak.TEXT_CENTER, EzSmartpeak.TEXT_BOLD));
+                printValues.put(EzSmartpeak.PrintText("My Company name", 2, EzSmartpeak.TEXT_CENTER, EzSmartpeak.TEXT_BOLD));
                 printValues.put(EzSmartpeak.EmptySpace());
                 printValues.put(EzSmartpeak.PrintText("INVOICE NO    : " + "8632588981149056", 2, EzSmartpeak.TEXT_LEFT));
                 printValues.put(EzSmartpeak.PrintText("TRANSACTION ID: " + "1000000002960639", 2, EzSmartpeak.TEXT_LEFT));
@@ -43,12 +43,12 @@ public class MainActivity extends AppCompatActivity {
                 printValues.put(EzSmartpeak.PrintText("AUTH CODE     : " + "12345678901920390091", 2, EzSmartpeak.TEXT_LEFT));
                 printValues.put(EzSmartpeak.PrintText("DATE/TIME     : " + "27/11/2019 08:46:53 AM", 2, EzSmartpeak.TEXT_LEFT));
                 printValues.put(EzSmartpeak.EmptySpace());
-                printValues.put(EzSmartpeak.PrintText("BOOST PAYMENT", 2, EzSmartpeak.TEXT_CENTER, EzSmartpeak.TEXT_BOLD));
+                printValues.put(EzSmartpeak.PrintText("E-WALLET PAYMENT", 2, EzSmartpeak.TEXT_CENTER, EzSmartpeak.TEXT_BOLD));
                 printValues.put(EzSmartpeak.EmptySpace());
                 printValues.put(EzSmartpeak.LineSplit());
                 printValues.put(EzSmartpeak.PrintText("TOTAL:         RM 1.00 ", 2, EzSmartpeak.TEXT_LEFT, EzSmartpeak.TEXT_BOLD));
                 printValues.put(EzSmartpeak.LineSplit());
-                printValues.put(EzSmartpeak.PrintDimensionObject(EzSmartpeak.DIMENSION_TWO, "HELLO", 3, EzSmartpeak.TEXT_CENTER, 1));
+                printValues.put(EzSmartpeak.PrintDimensionObject(EzSmartpeak.DIMENSION_TWO, "1000000002960639", 3, EzSmartpeak.TEXT_CENTER, 1));
                 printValues.put(EzSmartpeak.PrintText("**** CUSTOMER COPY ****", 2, EzSmartpeak.TEXT_CENTER));
                 EzSmartpeak.getPrint(printValues, R.drawable.m1paywallet_print, new EzSmartpeak.PrinterCallback() {
                     @Override
@@ -98,3 +98,5 @@ public class MainActivity extends AppCompatActivity {
 
     }
 }
+
+
